55b9ef8ec7e14d8b29de3f3e97dad9601fdc5a45
karlWinkler <karlw200@gmail.com>
2024-08-16 19:49:42 -0600

    feat: add user model to user app

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User as AuthUser
+
+class User(models.Model):
+    user = models.OneToOneField(AuthUser, on_delete=models.CASCADE)
+    preferences = models.TextField(default='')
+
+    image = models.ImageField(default='default.jpg', upload_to='profile_images')
+    bio = models.TextField(default='')
+
+    def __str__(self):
+        return f'{self.user.username}'
