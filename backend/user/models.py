f01bfff0c96ec571b9bb5f7b2a6b82e8432ed3f1
karlWinkler <karlw200@gmail.com>
2024-10-22 19:17:19 -0600

    fix: remove email from required fields

--- backend/user/models.py
+++ backend/user/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     first_name = models.TextField()
     last_name = models.TextField()
