05cac2c3406fa1e7cc9d94cb05ffa5dcfc9525ba
karlWinkler <karlw200@gmail.com>
2024-10-25 22:15:43 -0600

    PP-6: add more tests for create user endpoint

--- backend/user/tests/api/test_create_user.py
+++ backend/user/tests/api/test_create_user.py
@@ -13,3 +13,26 @@ def test_create_user_with_valid_data(client, user):
     response = client.post("/api/user/", json.dumps(data), content_type='application/json')
 
     assert response.status_code == 200
+    assert response.json["email"] == data["email"]
+
+
+@pytest.mark.django_db
+def test_does_not_create_user_with_invalid_data(client, user):
+    data = {}
+
+    response = client.post("/api/user/", json.dumps(data), content_type='application/json')
+
+    assert response.status_code == 422
+
+
+@pytest.mark.django_db
+def test_does_not_create_user_when_unauthenticated(client):
+    data = {
+        "email": "username@email.com",
+        "first_name": "user",
+        "last_name": "name",
+        "password": "user_password",
+    }
+    response = client.post("/api/user/", json.dumps(data), content_type='application/json')
+
+    assert response.status_code == 401
